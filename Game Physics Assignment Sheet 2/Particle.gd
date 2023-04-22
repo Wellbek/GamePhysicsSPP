2ca353c8420e482f230788c961da7685e590078c
Wellbek <louis.wellmeyer@gmail.com>
2023-04-22 19:41:53 +0200

    Sheet 2, Assignment 2: particle radius as constant variable

--- Game Physics Assignment Sheet 2/Particle.gd
+++ Game Physics Assignment Sheet 2/Particle.gd
@@ -7,21 +7,23 @@ export var velocity = Vector3(0,0,0)
 export var mass = 1.0
 
 var b = 10.25 # e)
+
+const pRadius = 0.2
 	
 func _physics_process(delta):
 	velocity = velocity + delta / mass * gravity
 	
-	if transform.origin.y <= 0.2:
-		transform.origin.y = 0.2
+	if transform.origin.y <= pRadius:
+		transform.origin.y = pRadius
 		velocity.y = - bounce * velocity.y
 		
 	# e) (Optional) constraints to prevent particles from penetrating walls
 	for axis in ['x', 'z']:
-		if transform.origin[axis] <= -b + 0.2:
-			transform.origin[axis] = -b + 0.2
+		if transform.origin[axis] <= -b + pRadius:
+			transform.origin[axis] = -b + pRadius
 			velocity[axis] = - bounce * velocity[axis]
-		elif transform.origin[axis] >= b - 0.2:
-			transform.origin[axis] = b - 0.2
+		elif transform.origin[axis] >= b - pRadius:
+			transform.origin[axis] = b - pRadius
 			velocity[axis] = - bounce * velocity[axis]
 		
 	transform.origin = transform.origin + delta*velocity
