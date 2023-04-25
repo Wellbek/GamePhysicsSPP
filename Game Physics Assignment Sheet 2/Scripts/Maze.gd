a62879b71782b9d0097110b65fbef93244abde1b
Fek <felixkern04@gmail.com>
2023-04-25 18:40:56 +0200

    Its now possible to rotate the maze with the mouse although collision is wonky. Also made sky grey. If you dont like it, change it back

--- Game Physics Assignment Sheet 2/Scripts/Maze.gd
+++ Game Physics Assignment Sheet 2/Scripts/Maze.gd
@@ -1,8 +1,10 @@
 extends RigidBody
 
 export var rotSpeed = 1.0
+export var mouseSpeed = 2
+export var clickOrigin = Vector2(0,0)
 
-func _process(delta):
+func _physics_process(delta):
 	var rotChange = Vector3(0,0,0)
 	
 	if Input.is_action_pressed("rot_left"):
@@ -18,6 +20,16 @@ func _process(delta):
 
 	rotate_object_local(Vector3(0,0,1),rotSpeed * delta * rotChange.z)
 	rotate_object_local(Vector3(1,0,0),rotSpeed * delta * rotChange.x)
+	
+	if Input.is_action_just_pressed("unlock_camera"):
+		clickOrigin = get_viewport().get_mouse_position()
+	
+	if Input.is_action_pressed("unlock_camera"):
+		var clickVec = get_viewport().get_mouse_position() - clickOrigin
+		clickOrigin = get_viewport().get_mouse_position()
+		clickVec = clickVec/10
+		rotate_object_local(Vector3(0,0,1),mouseSpeed * delta * -clickVec.x)
+		rotate_object_local(Vector3(1,0,0),mouseSpeed * delta * clickVec.y)
 	
 	#limit rotation along x,z and lock along y
 	var myRot = get_rotation()
