738ba1f046cfb002a8564daf98dd683768ad26b6
Wellbek <louis.wellmeyer@gmail.com>
2023-05-05 16:12:12 +0200

    Tunneling fixed (mostly) through increased physics fps (see project settings) and by clamping arrowkey rotation and mouse rotation to a set maxRotPerFrame variable (see Maze.gd)

--- Game Physics Assignment Sheet 3/Scripts/Maze.gd
+++ Game Physics Assignment Sheet 3/Scripts/Maze.gd
@@ -1,8 +1,9 @@
 extends RigidBody
 
-var rotSpeed = .3 # NOTE: Higher values than 0.3 will lead to tunneling (object passing through collider)
-export var mouseSpeed = 2.0
+var arrowkeySpeed = 0.7
+export var mouseSpeed = 5.0
 export var clickOrigin = Vector2(0,0)
+export var maxRotPerFrame = .01 # avoids tunneling when rotating rapidly (the lower the framerate the higher the variables value should be)
 
 export var pathToNewScene = "res://Scenes/MarbleMaze.tscn"
 
@@ -22,21 +23,19 @@ func _physics_process(delta):
 	
 	rotChange = rotChange.normalized()
 
-	rotate_object_local(Vector3(0,0,1),rotSpeed * delta * rotChange.z)
-	rotate_object_local(Vector3(1,0,0),rotSpeed * delta * rotChange.x)
+	rotate_object_local(Vector3(0,0,1),clamp(arrowkeySpeed * delta * rotChange.z, -maxRotPerFrame, maxRotPerFrame))
+	rotate_object_local(Vector3(1,0,0),clamp(arrowkeySpeed * delta * rotChange.x, -maxRotPerFrame, maxRotPerFrame))
 	# ======================================
 	# Mouse Rotation:
-	# NOTE: Tunneling!
-	#		simply clamping mouseSpeed and/or clickVector wont resolve the issue as in the arrow key case
 	if Input.is_action_just_pressed("unlock_camera"):
 		clickOrigin = get_viewport().get_mouse_position()
 	
 	if Input.is_action_pressed("unlock_camera"):
 		var clickVec = get_viewport().get_mouse_position() - clickOrigin
 		clickOrigin = get_viewport().get_mouse_position()
-		clickVec = clickVec/10
-		rotate_object_local(Vector3(0,0,1),mouseSpeed * delta * -clickVec.x)
-		rotate_object_local(Vector3(1,0,0),mouseSpeed * delta * clickVec.y)
+		clickVec = clickVec
+		rotate_object_local(Vector3(0,0,1),clamp(mouseSpeed * delta * -clickVec.x, -maxRotPerFrame, maxRotPerFrame))
+		rotate_object_local(Vector3(1,0,0),clamp(mouseSpeed * delta * clickVec.y, -maxRotPerFrame, maxRotPerFrame))
 	# ======================================
 	
 	#limit rotation along x,z and lock along y
