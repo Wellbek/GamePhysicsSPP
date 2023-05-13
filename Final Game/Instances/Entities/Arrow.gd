4b9bf158cda0db08779d102c6a2fb0962d3034af
Wellbek <louis.wellmeyer@gmail.com>
2023-05-13 13:17:04 +0200

    Can now shoot arrows (very simple)

---
+++ Final Game/Instances/Entities/Arrow.gd
@@ -0,0 +1,6 @@
+extends RigidBody
+
+func _process(delta):
+	# TODO: Arrow realistisch rotieren
+	# mby hilfreich: https://www.youtube.com/watch?v=3gHdnF1FZ1w (das ist 2D)
+	pass
--- Final Game/Scripts/Player/Combat.gd
+++ Final Game/Scripts/Player/Combat.gd
@@ -3,12 +3,25 @@ extends Spatial
 
 onready var bow_animator = $AnimationPlayer
 
-func _process(delta):
+onready var arrow_scene = load("res://Instances//Entities//arrow.tscn")
+
+var arrow_impulse_multiplier = 50
+
+func _process(delta):	
 	if Input.is_action_just_pressed("shoot"):
 		bow_animator.play("bow_draw_animation")
 		
 	if Input.is_action_just_released("shoot"):
+		# Shoot the projectile 
+		var arrow = arrow_scene.instance()
+		
+		arrow.transform.origin = get_parent().global_transform.origin
+		arrow.rotation = get_parent().global_transform.basis.get_euler()
+		arrow.get_node("RigidBody").apply_central_impulse(-get_parent().global_transform.basis.z * arrow_impulse_multiplier * bow_animator.current_animation_position/bow_animator.current_animation_length)
+		
+		
+		# reset bow animation
 		bow_animator.stop(true) # stops current animation
 		bow_animator.seek(0, true) # resets animation to default
 		
-		# To Do: Shoot the actual projectile here
+		get_tree().root.add_child(arrow)
