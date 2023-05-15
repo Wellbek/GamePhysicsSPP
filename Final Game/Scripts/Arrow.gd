d84915b9d9e0de0b7a77ac9965dbd048931fe535
Wellbek <louis.wellmeyer@gmail.com>
2023-05-15 22:16:41 +0200

    Arrow now rotating but not correctly...

---
+++ Final Game/Scripts/Arrow.gd
@@ -0,0 +1,13 @@
+extends RigidBody
+
+var child = null
+
+func _ready():
+	child = get_child(0);
+
+func _process(delta):
+	# works kinda. however, the max/min rot we can get from start is like +/-25
+	var vec = Vector2(max(-global_transform.basis.inverse().xform(linear_velocity).z, 0),  linear_velocity.y)
+	var rot = int(rad2deg(vec.angle()))
+	if child.rotation.x != rot:
+		child.rotation_degrees.x = rot
