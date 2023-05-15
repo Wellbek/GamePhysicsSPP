ecb215f463442961a3a964e29d598cc035a6c0b9
Wellbek <louis.wellmeyer@gmail.com>
2023-05-16 00:36:59 +0200

    Arrow now behaving realistically

--- Final Game/Scripts/Arrow.gd
+++ Final Game/Scripts/Arrow.gd
@@ -6,8 +6,4 @@ func _ready():
 	child = get_child(0);
 
 func _process(delta):
-	# works kinda. however, the max/min rot we can get from start is like +/-25 (and just doesnt rlly look right in game)
-	var vec = Vector2(max(-global_transform.basis.inverse().xform(linear_velocity).z, 0),  linear_velocity.y)
-	var rot = int(rad2deg(vec.angle()))
-	if child.rotation.x != rot:
-		child.rotation_degrees.x = rot
+	child.look_at(global_transform.origin + linear_velocity, Vector3.UP)
