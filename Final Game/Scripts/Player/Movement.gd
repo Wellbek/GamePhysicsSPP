f9a32287b0a79084000a194e459149e8253648ab
Fek <felixkern04@gmail.com>
2023-05-16 10:55:11 +0200

    Fixed mouse going outside of window

--- Final Game/Scripts/Player/Movement.gd
+++ Final Game/Scripts/Player/Movement.gd
@@ -1,23 +1,32 @@
 extends Spatial
 
 export var mouse_sens = 5.0
-export var clickOrigin = Vector2(0,0)
+var clickVec = Vector2(0,0)
+
+var pending_rotation = false
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN) # hide mouse by default
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) #locks mouse to the center of screen
+	Input.set_use_accumulated_input(false) #makes Input events emit as often as possible => smoother rotation
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		clickVec = event.relative/10 # with InputEventMouseMotion.relative we get the amount the mouse moved in the last frame
+		pending_rotation = true
+			
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("show_mouse"):
-		if Input.get_mouse_mode() == Input.MOUSE_MODE_HIDDEN:
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		else: Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	# This isnt possible anymore because it would override the MOUSE_MODE_CAPTURED
+	#if Input.is_action_just_pressed("show_mouse"):
+	#	if Input.get_mouse_mode() == Input.MOUSE_MODE_HIDDEN:
+	#		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	#	else: Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 	# Mouse Rotation:
-	var clickVec = get_viewport().get_mouse_position() - clickOrigin
-	clickOrigin = get_viewport().get_mouse_position()
-	clickVec = clickVec / 10
-	rotate_object_local(Vector3(0,1,0),mouse_sens * delta * -clickVec.x)
-	get_node("FirstPersonCamera").rotate_object_local(Vector3(1,0,0),mouse_sens * delta * -clickVec.y)
+	if pending_rotation:
+		rotate_object_local(Vector3(0,1,0),mouse_sens * delta * -clickVec.x)
+		get_node("FirstPersonCamera").rotate_object_local(Vector3(1,0,0),mouse_sens * delta * -clickVec.y)
+		pending_rotation = false
 
 	# limit camera rotation along x (so we can only look up/down a certain amount)
 	var myRot = get_node("FirstPersonCamera").get_rotation()
