c4256b514603bd479bba0dd70bd74331c78ce9d5
Fek <felixkern04@gmail.com>
2023-05-17 16:58:29 +0200

    Movement: Jump input buffer

--- Final Game/Scripts/Player/Movement.gd
+++ Final Game/Scripts/Player/Movement.gd
@@ -7,6 +7,11 @@ export var mouse_sens = 5.0
 var clickVec = Vector2(0,0)
 var velocity = Vector3(0,0,0)
 
+var jump_timer = 0
+export var jump_buffer_time = 0.1
+var can_press_jump = true
+var jump_buffered = false
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) #locks mouse to the center of screen
 	#Input.set_use_accumulated_input(false) #makes Input events emit as often as possible => smoother rotation
@@ -21,12 +26,17 @@ func _input(event):
 
 func _physics_process(delta):
 	
-	velocity.y += gravity
-	if Input.is_action_just_pressed("jump") && is_on_floor():
-		velocity.y = jump_strength
+	handle_jump(delta)
 	
-	velocity = move_and_slide(velocity, Vector3(0,1,0))
-	
+	handle_wasd(delta)
+
+	# limit camera rotation along x (so we can only look up/down a certain amount)
+	var myRot = get_node("FirstPersonCamera").get_rotation()
+	get_node("FirstPersonCamera").rotation.x = clamp(myRot.x, -PI/2, 1.2)
+
+
+#------------------------------ helper methods -----------------------------------
+func handle_wasd(delta):
 	var change = Vector3(0,0,0)
 	if Input.is_action_pressed("walk_left"):
 		change.x -= 1
@@ -40,6 +50,22 @@ func _physics_process(delta):
 	change = change.normalized()
 	translate_object_local(change*delta*movement_speed)
 
-	# limit camera rotation along x (so we can only look up/down a certain amount)
-	var myRot = get_node("FirstPersonCamera").get_rotation()
-	get_node("FirstPersonCamera").rotation.x = clamp(myRot.x, -1.2, 1.2)
+func handle_jump(delta):
+	
+	if jump_timer <= jump_buffer_time:
+		jump_timer += delta
+	else:
+		jump_buffered = false
+	
+	if Input.is_action_pressed("jump") && !is_on_floor():
+		jump_buffered = true
+		jump_timer = 0
+	
+	velocity.y += gravity
+	if (Input.is_action_just_pressed("jump") || jump_buffered) && is_on_floor():
+		velocity.y = jump_strength
+	
+	#last parameter determines whether the player can move rigidbodies or view then as static bodies
+	#the third and fourth parameters are the default values but i dont know how to keep them while also changing the last lol
+	#first parameter is linear_velocity that is getting applied, second is the normal of the floor
+	velocity = move_and_slide(velocity, Vector3(0,1,0), false, 4, 0.785398, false)
