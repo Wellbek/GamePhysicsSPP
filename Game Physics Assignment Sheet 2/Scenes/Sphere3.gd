2ed73c0d0bae5bfdc7bcc155d6fd7122c4788724
Louis Wellmeyer <54329793+Wellbek@users.noreply.github.com>
2023-05-26 13:04:38 +0200

    Deleted accidentally uploaded test file for sheet 2

--- Game Physics Assignment Sheet 2/Scenes/Sphere3.gd
+++
@@ -1,9 +0,0 @@
-extends RigidBody
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	print("Pos:", transform.origin)
-	print("lv:", linear_velocity)
-	print("av:", angular_velocity)
-	print("Rot:", rotation_degrees)
-	print("----------------------")
