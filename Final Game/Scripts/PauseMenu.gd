1354145765f9b97090c7f4a26ccb2186294d4468
Fek <felixkern04@gmail.com>
2023-06-08 12:18:18 +0200

    Very basic pause menu with mouse sensitivity slider, still needs a lot of love

---
+++ Final Game/Scripts/PauseMenu.gd
@@ -0,0 +1,29 @@
+extends Panel
+
+
+onready var upgrade_panel = PlayerVariables.gui.get_node("UpgradePanel")
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel") && !upgrade_panel.visible:
+		if visible:
+			unpause()
+		else:
+			pause()
+
+func pause():
+	show()
+	get_tree().paused = true
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+func unpause():
+	hide()
+	get_tree().paused = false
+	get_tree().paused = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _on_UnpauseButton_button_up():
+	unpause()
+
+
+func _on_MouseSensSlider_value_changed(value):
+	PlayerVariables.player.mouse_sens = value
