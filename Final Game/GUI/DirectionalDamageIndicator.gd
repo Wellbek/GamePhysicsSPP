4dc518da2b7271f53eb409ca6de1cf5c993f6fd4
Wellbek <louis.wellmeyer@gmail.com>
2023-06-09 16:19:32 +0200

    Added directional damage indicator

---
+++ Final Game/GUI/DirectionalDamageIndicator.gd
@@ -0,0 +1,4 @@
+extends TextureRect
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	queue_free()
--- Final Game/Scripts/Core.gd
+++ Final Game/Scripts/Core.gd
@@ -3,9 +3,13 @@ extends RigidBody
 export var max_health = 100.0
 onready var health = max_health
 
-onready var core_bar = PlayerVariables.gui.get_node("TopBar/CoreBar")
+onready var gui = PlayerVariables.gui
+onready var core_bar = gui.get_node("TopBar/CoreBar")
 
 var dead = false
+
+
+onready var indicator_scene = load("res://Instances//GUI//DirectionalDamageIndicator.tscn")
 
 export var debug = false
 
@@ -26,6 +30,21 @@ func take_damage(var amount: float):
 	if health <= 0:
 		health = 0
 		die()
+	
+	var indicator = indicator_scene.instance()
+	
+	gui.add_child(indicator)
+	gui.move_child(indicator,0) # render it below everything else so that indicator texture doesnt block any buttons
+
+	var player_transform = PlayerVariables.player.global_transform
+	var player_forward = -player_transform.basis.z
+	var direction = global_transform.origin - player_transform.origin
+	
+	var angle_rad = atan2(direction.x, direction.z) - atan2(player_forward.x, player_forward.z)
+	if angle_rad < 0:
+		angle_rad += 2 * PI
+
+	indicator.set_rotation(-angle_rad)
 	
 	update_core_bar()
 		
